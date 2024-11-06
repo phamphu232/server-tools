a9bf2db7dede6041c3b8815ad3e859e5a957ed7a
Phu Paul <ptphu@monotos.biz>
2024-11-06 10:34:02 +0700

    Change time report

--- app/client/schedule.sh
+++ app/client/schedule.sh
@@ -5,7 +5,7 @@ APP_DIR=$(dirname "$0")
 CURRENT_MINUTE=$(date +%-M)
 
 # Every 2 minutes
-if [ $((CURRENT_MINUTE % 2)) -eq 0 ]; then
+if [ $((CURRENT_MINUTE % 1)) -eq 0 ]; then
     echo "Running $APP_DIR/client.sh"
 
     if [ -z "$1" ]; then
