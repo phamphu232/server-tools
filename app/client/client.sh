5ce18973e6c59e07400023669fbdc21ab614aa22
Phu Paul <ptphu@monotos.biz>
2024-11-15 11:21:43 +0700

    Up

--- app/client/client.sh
+++ app/client/client.sh
@@ -41,9 +41,9 @@ if [ -z "$PLATFORM" ]; then
     PUBLIC_IP=$(curl -4 -s http://ifconfig.me)
 fi
 
-CPU_TOP=$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 31 | awk '{printf "%s\\n", $0}')
+CPU_TOP=$(ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head -n 31 | awk '{printf "%s\\n", $0}')
 
-RAM_TOP=$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -n 31 | awk '{printf "%s\\n", $0}')
+RAM_TOP=$(ps -eo pid,ppid,cmd,%cpu --sort=-%mem | head -n 31 | awk '{printf "%s\\n", $0}')
 
 CPU=$(top -bn1 | grep -i "Cpu(s)")
 
